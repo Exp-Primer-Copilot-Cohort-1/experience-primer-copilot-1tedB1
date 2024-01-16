909d35a847931e8a0f54bd6c4323c779d781be64
ZEESHAN ASHIR MEHMOOD <132244973+1tedB1@users.noreply.github.com>
2024-01-16 10:47:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,19 @@
 //create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
+
+//function to connect to database
+const db = require('./db');
+function connectDB(){
+    db.connect((err)=>{
+        if(err){
+            console.log('unable to connect to database');
+            process.exit(1);
+        }else{
+            console.log('connected to database');
+        }
+    })
+}
